36ebdcd6f38911b72ba25a6ceb9b77cf533f4e4a
Vincent Plateau <143662661+Bixente-san@users.noreply.github.com>
2025-01-10 16:59:20 +0100

    Update portfolio_streamlit.py

--- portfolio_streamlit.py
+++ portfolio_streamlit.py
@@ -1061,11 +1061,7 @@ def send_email(name, email, message):
 
 def contact_page():
     st.title("📫 Contact")
-    st.markdown("##### ✉️ Email: vi.plateau@gmail.com")
-    st.markdown(f"#####  Linkedin: [Vincent PLATEAU](https://linkedin.com/in/vincent-plateau/)")
-    st.write("LinkedIn: [Vincent PLATEAU](https://linkedin.com/in/vincent-plateau/)")
-    st.markdown("[:octopus: GitHub Repository](your_github_link_here)")
-   
+    
     github_icon = """
     <svg viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
@@ -1079,15 +1075,17 @@ def contact_page():
     """
     
     with st.container():
-        col1, col2, col3, col4 = st.columns([0.1, 1.5, 0.1, 1.5])
+        col1, col2, col3, col4, col5 = st.columns([1.5, 0.1, 1.5, 0.1, 1.5])
         with col1:
-            st.markdown(f'<div style="width: 24px; height: 24px;">{github_icon}</div>', unsafe_allow_html=True)
+            st.markdown("##### ✉️ Email: vi.plateau@gmail.com")
         with col2:
-            st.markdown("GitHub: [https://github.com/Bixente-san](https://github.com/Bixente-san)")
-        with col3:
             st.markdown(f'<div style="width: 24px; height: 24px;">{linkedin_icon}</div>', unsafe_allow_html=True)
+        with col3:
+            st.markdown("##### LinkedIn: [https://linkedin.com/in/vincent-plateau/](https://linkedin.com/in/vincent-plateau/)")
         with col4:
-            st.markdown("LinkedIn: [https://linkedin.com/in/vincent-plateau/](https://linkedin.com/in/vincent-plateau/)")
+            st.markdown(f'<div style="width: 24px; height: 24px;">{github_icon}</div>', unsafe_allow_html=True)
+        with col5:
+            st.markdown("##### GitHub: [https://github.com/Bixente-san](https://github.com/Bixente-san)")
 
     
     
