0aadcfd8b6aaa7ddd780e194570eb7928e7c26e0
Vincent Plateau <143662661+Bixente-san@users.noreply.github.com>
2025-01-10 17:07:26 +0100

    Update portfolio_streamlit.py

--- portfolio_streamlit.py
+++ portfolio_streamlit.py
@@ -1076,7 +1076,7 @@ def contact_page():
     
     email_icon = """
     <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M2 6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6zm2-1h16a1 1 0 0 1 1 1v.586l-8.293 8.293a1 1 0 0 1-1.414 0L3 6.586V6a1 1 0 0 1 1-1zm16 2.414V18a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V7.414l7.293 7.293a3 3 0 0 0 4.414 0L20 7.414z"/>
+        <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/>
     </svg>
     """
 
